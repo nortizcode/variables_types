56d10ac30ac27c6e44ce2d30ae53d5d7de117788
Nathan Ortiz <nkortiz92@gmail.com>
2020-02-26 14:26:34 -0700

    variables-and-data prework. made a javascript file

---
+++ variables-and-data.js
@@ -0,0 +1,15 @@
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);
+
+let add = 1+2;
+let sub = 2-1;
+let mult = 2*4;
+let division= 4/2;
+let mod = 5 % 2;
+
+let string = "string 1 " + "string 2 ";
+
+console.log(string)
